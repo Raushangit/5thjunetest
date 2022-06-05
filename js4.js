36e6e814b280a2215c4007c5ad2ad9466ae70a1d
Raushangit <100925852+Raushangit@users.noreply.github.com>
2022-06-05 13:14:32 +0530

    Add files via upload

---
+++ js4.js
@@ -0,0 +1,42 @@
+const Name = `raushan`;
+const greeting = `good morning`;
+console.log(greeting + ' ' + Name);
+
+let html;
+html = "<h1> this is heading</h1>" +
+       "<p> this is my paragrapg </p>";
+
+    //    html = html.concat('this', 'str2');
+    //    console.log(html);
+    //    console.log(html.length);
+    //    console.log(html.toLowerCase());
+    //    console.log(html.toUpperCase());
+    //    console.log(html);
+
+
+
+    //    console.log(html[5]);
+    //    console.log(html.indexOf('<'));
+    //    console.log(html.lastIndexOf('<'));
+    //    console.log(html.charAt(8));
+    //    console.log(html.endsWith('ing'));
+
+
+    //    console.log(html.includes('pg'));
+    //    console.log(html.substring(1,7));
+    //    console.log(html.slice(0,5));
+    //    console.log(html.split('is'));
+    //    console.log(html.replace('my', 'your'));
+       
+
+       let fruit1 = 'orange\'';
+       let fruit2 = 'apple';
+
+       let myHtml = `Hello ${name}
+        <h1> this is my heading </h>
+        <p> you like ${fruit1} and ${fruit2}`;
+
+        document.body.innerHTML = myHtml;
+
+
+
